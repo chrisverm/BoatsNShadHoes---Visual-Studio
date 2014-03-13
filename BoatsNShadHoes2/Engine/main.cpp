9a72001a8d33dd0ac8c32fa2aef18f5e8a1fa685
Starfishheat <eap4247@rit.edu>
2014-03-13 11:47:02 -0400

    Sorted Folders
    Yeah

---
+++ BoatsNShadHoes2/Engine/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "WICTextureLoader.h"
+
+int main()
+{
+	std::cout << " HI! \n " << std::endl;
+
+#ifdef DEBUG
+	std::cout << " DEBUG! \n" << std::endl;
+#endif
+
+	getchar();
+
+	return 0;
+}
